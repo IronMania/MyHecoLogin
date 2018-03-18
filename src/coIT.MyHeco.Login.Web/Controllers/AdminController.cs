e1f8affd3417c50e570eba14e9e87dd27118d6a4
Jörg Weißbecker <jw@co-IT.eu>
2018-03-18 09:29:03 +0100

    removed siren

--- src/coIT.MyHeco.Login.Web/Controllers/AdminController.cs
+++ src/coIT.MyHeco.Login.Web/Controllers/AdminController.cs
@@ -1,8 +1,6 @@
 ﻿using System.Linq;
 using coIT.MyHeco.Login.Domain;
 using coIT.MyHeco.Login.Domain.Services;
-using coIT.MyHeco.Login.Web.Extensions;
-using coIT.MyHeco.Login.Web.Model.Hypermedia;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coIT.MyHeco.Login.Web.Controllers
@@ -11,12 +9,9 @@ namespace coIT.MyHeco.Login.Web.Controllers
     public class AdminController : Controller
     {
         private readonly IMyHecoRepository _myHecoRepository;
-        private readonly IUrlHelper _urlHelper;
 
-        public AdminController(IUrlHelper urlHelper,
-            IMyHecoRepository myHecoRepository)
+        public AdminController(IMyHecoRepository myHecoRepository)
         {
-            _urlHelper = urlHelper;
             _myHecoRepository = myHecoRepository;
         }
 
@@ -24,22 +19,9 @@ namespace coIT.MyHeco.Login.Web.Controllers
         [HttpGet("Users")]
         public IActionResult GetAll()
         {    
-            var result = new Siren(_urlHelper);
-            var benutzer = _myHecoRepository.All().OfType<NichtAktivierterBenutzer>()
-                .Select(ConvertToSiren);
-            result.Entities.AddRange(benutzer);
-            return Json(result);
+            var benutzer = _myHecoRepository.All().OfType<NichtAktivierterBenutzer>();
+            return Json(benutzer.Select(siren => new {email = siren.Email, code = siren.AktivierungsCode}));
             
-        }
-
-        private Siren ConvertToSiren(NichtAktivierterBenutzer user)
-        {
-            var selfUrl = _urlHelper.AbsoluteAction(nameof(LoginLevel2Controller.Search), "Login", new {email = user.Email});
-            var result = new Siren(selfUrl);
-            result.Class.Add(user.GetType().Name);
-            result.Properties.Add("Email",user.Email);
-            result.Properties.Add("ActivationCode",user.AktivierungsCode);
-            return result;
         }
     }
 }
