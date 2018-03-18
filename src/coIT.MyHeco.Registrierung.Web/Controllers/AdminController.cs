2111a932f584cefa351ef7316b782a3355f94be0
Jörg Weißbecker <jw@co-IT.eu>
2018-03-18 09:31:05 +0100

    removed siren

--- src/coIT.MyHeco.Registrierung.Web/Controllers/AdminController.cs
+++ src/coIT.MyHeco.Registrierung.Web/Controllers/AdminController.cs
@@ -1,8 +1,6 @@
 ﻿using System.Linq;
 using coIT.MyHeco.Registrierung.Domain;
 using coIT.MyHeco.Registrierung.Domain.Services;
-using coIT.MyHeco.Registrierung.Web.Extensions;
-using coIT.MyHeco.Registrierung.Web.Model.Hypermedia;
 using Microsoft.AspNetCore.Mvc;
 
 namespace coIT.MyHeco.Registrierung.Web.Controllers
@@ -11,35 +9,21 @@ namespace coIT.MyHeco.Registrierung.Web.Controllers
     public class AdminController : Controller
     {
         private readonly INichtAktivierteBenutzerRepository _nichtAktivierteBenutzerRepository;
-        private readonly IUrlHelper _urlHelper;
 
         public AdminController(IUrlHelper urlHelper,
             INichtAktivierteBenutzerRepository nichtAktivierteBenutzerRepository)
         {
-            _urlHelper = urlHelper;
             _nichtAktivierteBenutzerRepository = nichtAktivierteBenutzerRepository;
         }
 
         // GET api/values
         [HttpGet("Users")]
         public IActionResult GetAll()
-        {    
-            var result = new Siren(_urlHelper);
-            var benutzer = _nichtAktivierteBenutzerRepository.All().OfType<NichtAktivierterBenutzer>()
-                .Select(ConvertToSiren);
-            result.Entities.AddRange(benutzer);
-            return Json(result);
+        {
+            var benutzer = _nichtAktivierteBenutzerRepository.All().OfType<NichtAktivierterBenutzer>();
+            return Json(benutzer.Select(aktivierterBenutzer => new {email = aktivierterBenutzer.Email, code = aktivierterBenutzer.AktivierungsCode}));
             
         }
 
-        private Siren ConvertToSiren(NichtAktivierterBenutzer user)
-        {
-            var selfUrl = _urlHelper.AbsoluteAction(nameof(RegisterLevel2Controller.Search), typeof(RegisterLevel2Controller).ControllerName(), new {email = user.Email});
-            var result = new Siren(selfUrl);
-            result.Class.Add(user.GetType().Name);
-            result.Properties.Add("Email",user.Email);
-            result.Properties.Add("ActivationCode",user.AktivierungsCode);
-            return result;
-        }
     }
 }
